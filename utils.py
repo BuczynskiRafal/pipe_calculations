0c36cd67679b6ee34ac7aea881701daabf6a11b8
BuczynskiRafal <buczynski.rafal2@gmail.com>
2022-07-27 18:06:55 +0200

    finish caclulation method

--- utils.py
+++ utils.py
@@ -55,6 +55,8 @@ def calc_cross_sectional_area(h, d):
             return math.pi * radius ** 2 - (1 / 2 * (alpha - math.sin(alpha)) * radius ** 2)
         elif h == radius:
             return math.pi * radius ** 2 / 2
+        elif h == d:
+            return math.pi * radius ** 2
         else:
             return 1 / 2 * (alpha - math.sin(alpha)) * radius ** 2
     else:
@@ -70,7 +72,11 @@ def calc_u(h, d):
         radius = d / 2
         # cięciwa
         chord = math.sqrt((radius ** 2 - (h-radius) ** 2)) * 2
+        print(f"chord: {chord}")
         angle = math.degrees(math.acos((radius ** 2 + radius ** 2 - chord ** 2) / (2 * radius ** 2)))
+        print(f"angle: {angle}")
+        if h > radius:
+            return 2 * math.pi * radius - (angle / 360 * 2 * math.pi * radius)
         return angle / 360 * 2 * math.pi * radius
     return None
 
@@ -83,18 +89,22 @@ def calc_rh(f, u):
 
 
 def calc_velocity(h, d, i):
-    """
-    This function takes three arguments, `c`, `rh`, and `i`, and returns the velocity of water inside pipe.
-
-    :param c: coefficient calculated according to Manning's formula
-    :param rh: hydraulic radius, equal to the ratio of the cross-sectional area to the wetted circumference [m]
-    :param i: decrease of the wastewater table, equal to the slope of the bottom of the sewer when the liquid flows with a free mirror, or the decrease of the pressure line when the sewer works under pressure,
-    :return: The velocity of the wave.
-    """
     if validate_filling(h, d):
+        print(f"area: {math.pi * (d/2) ** 2}")
+        print(f"obwód: {2 * math.pi * d/2}")
         f = calc_cross_sectional_area(h, d)
+        print(f"f: {f}")
         u = calc_u(h, d)
-        return (1 / 0.013) * (calc_rh(f, u) ** (2/3)) * i ** (1/2)
+        print(f"u: {u}")
+        rh = calc_rh(f, u)
+        print(f"rh: {rh}")
+        print(f"i: {i}")
+        a = 1 / 0.013
+        print(f"a: {a}")
+        vv = (1/0.013) * rh ** (1/6) * math.sqrt(rh * i)
+        print(f"vv: {vv}")
+        print(f"filling: {calc_filling_percentage(h, d)}")
+        return a * rh ** (2/3) * i ** 0.5
     return None
 
 
@@ -114,14 +124,14 @@ def calc_flow(h, d, i):
 
 # mam podany przepływ i spadek, chcę sprawdzić czy dobrano poprawnie średnicę
 # ma zwrócić minimalną średnicę
-def validate_diameter(q, d, i):
-    # d - dobrana średnica
-    # q - aktualny przepływ
-    # i - dobrany spadek
+# def validate_diameter(q, d, i):
+#     # d - dobrana średnica
+#     # q - aktualny przepływ
+#     # i - dobrany spadek
 
-    # 1 sprawdzić napełnienie kanału. - obliczyć h na podstawie przepływu średnicy i spadku
-    # 1 oblicz prędkość wody
-    v = calc_velocity()
+#     # 1 sprawdzić napełnienie kanału. - obliczyć h na podstawie przepływu średnicy i spadku
+#     # 1 oblicz prędkość wody
+#     v = calc_velocity()
 
 
 def find_h(q, d, i):
@@ -134,6 +144,10 @@ def find_h(q, d, i):
             print(f"q: {q}, flow: {flow:.4f}, h: {h:.4f}")
     return h
 
+# v = calc_velocity(0.16, 0.2, 0.02)
+# print(f"v: {v}")
+q = calc_flow(0.16, 0.2, 0.02)
+print(f"q: {q*1000}")
 
 def insert_excel_data(path):
     data = pd.read_excel(path)
@@ -149,7 +163,7 @@ def insert_excel_pipe_settings(path):
     return df
 
 # print(insert_excel_data('pipes.xlsx'))
-print(insert_excel_pipe_settings(path='pipe_settings.xlsx'))
+# print(insert_excel_pipe_settings(path='pipe_settings.xlsx'))
 
 
 
